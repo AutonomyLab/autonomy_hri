e69ae147e0cedc2ad03088a645205c5e29918cc1
Mani Monajjemi <mani.monajjemi@gmail.com>
2012-07-06 13:40:09 -0700

    Init functions ported, compiled well

--- src/autonomy_human.cpp
+++ src/autonomy_human.cpp
@@ -11,6 +11,7 @@
 #include "opencv2/video/tracking.hpp"
 
 using namespace cv;
+using namespace std;
 
 class CFaceTracker
 {
@@ -23,6 +24,9 @@ public:
 	};
 	
 private:
+	bool isInited;
+	int iWidth;
+	int iHeight;
 	//ros::NodeHandle node;
 	Mat rawFrame;
 	Rect searchROI;
@@ -42,21 +46,26 @@ private:
 	float maxRejectCov;
 
 	// Histograms (Skin)
+	MatND faceHist;
 	int hbins;
 	int sbins;
-	MatND faceHist;
-
 	Mat skin;
 	Mat skinPrior;
 	Mat skinImage;
 	
+	string strStates[4];
+	
+	void copyKalman(KalmanFilter* src, KalmanFilter* dest);
+	
 public:
 	CFaceTracker(int _initialScoreMin, int _initialDetectFrames, int _initialRejectFrames);
 	void visionCallback(const sensor_msgs::ImageConstPtr& frame);
+	void reset();
 };
 
 CFaceTracker::CFaceTracker(int _initialScoreMin, int _initialDetectFrames, int _initialRejectFrames)
 {
+	isInited = false;
 	stateCounter = 0;
 	
 	initialScoreMin = _initialScoreMin;
@@ -67,6 +76,69 @@ CFaceTracker::CFaceTracker(int _initialScoreMin, int _initialDetectFrames, int _
 	
 	hbins = 15;
 	sbins = 16;
+	
+	// x,y,xdot,ydot,w,h
+	state = Mat::zeros(6, 1, CV_32F); 
+	
+	// x,y,w,h
+	measurement = Mat::zeros(4, 1, CV_32F);
+	
+	strStates[0] = "NOFACE";
+	strStates[1] = "DETECT";
+	strStates[2] = "TRACKG";
+	strStates[3] = "REJECT";
+}
+
+void CFaceTracker::copyKalman(KalmanFilter* src, KalmanFilter* dest)
+{
+	dest->measurementNoiseCov = src->measurementNoiseCov.clone();
+	dest->controlMatrix = src->controlMatrix.clone();
+	dest->errorCovPost = src->errorCovPost.clone();
+	dest->errorCovPre = src->errorCovPre.clone();
+	dest->gain = src->gain.clone();
+	dest->measurementMatrix = src->measurementMatrix.clone();
+	dest->processNoiseCov = src->processNoiseCov.clone();
+	dest->statePost = src->statePost.clone();
+	dest->statePre = src->statePre.clone();
+	dest->transitionMatrix = src->transitionMatrix.clone();
+}
+
+void CFaceTracker::reset()
+{
+	searchROI = Rect(Point(0,0), Point(iWidth,iHeight));
+	measurement.setTo(Scalar(0));
+	KFTracker->transitionMatrix = * (Mat_<float>(6,6) 
+			<< 
+			1,0,1,0,0,0,
+			0,1,0,1,0,0, 
+			0,0,1,0,0,0,
+			0,0,0,1,0,0,
+			0,0,0,0,1,0,
+			0,0,0,0,0,1
+			);
+
+	KFTracker->measurementMatrix = *(Mat_<float>(4,6) 
+			<< 
+			1,0,0,0,0,0,
+			0,1,0,0,0,0,
+			0,0,0,0,1,0,
+			0,0,0,0,0,1
+			);
+
+	KFTracker->statePre = *(Mat_<float>(6,1) << 0,0,0,0,0,0);
+	KFTracker->statePost = *(Mat_<float>(6,1) << 0,0,0,0,0,0);
+	
+	setIdentity(KFTracker->errorCovPre, Scalar_<float>::all(1e2));
+	setIdentity(KFTracker->errorCovPost, Scalar_<float>::all(1e2));
+	setIdentity(KFTracker->processNoiseCov, Scalar_<float>::all(0.05));
+	setIdentity(KFTracker->measurementNoiseCov, Scalar_<float>::all(1.0));
+
+	copyKalman(KFTracker, MLSearch);
+	
+
+	skinPrior = Scalar::all( 1.0 / (iWidth * iHeight)); 
+	skin = Scalar::all(1.0 / (skin.rows * skin.cols));
+	faceHist = Scalar::all(1.0 / (faceHist.rows * faceHist.cols) );
 }
 
 void CFaceTracker::visionCallback(const sensor_msgs::ImageConstPtr& frame)
@@ -81,6 +153,15 @@ void CFaceTracker::visionCallback(const sensor_msgs::ImageConstPtr& frame)
         ROS_ERROR("Error converting the input image: %s", e.what());
         return;
     }
+	
+	if (isInited == false)
+	{
+		// Get Image Info from the first frame
+		isInited = true;
+		iWidth = cv_ptr->image.cols;
+		iHeight = cv_ptr->image.rows;
+		reset();
+	}
 	
 	namedWindow("test");
 	imshow("test",cv_ptr->image);
