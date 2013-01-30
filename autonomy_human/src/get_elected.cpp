6d03e8029bac434c00dba2c6f3f8b243ca77cfec
Shokoofeh <sh.pourmehr@gmail.com>
2013-01-29 16:59:57 -0800

    Add first voice commands.

--- autonomy_human/src/get_elected.cpp
+++ autonomy_human/src/get_elected.cpp
@@ -18,12 +18,19 @@ using namespace std;
 using namespace cv;
 #define ELECTION_TIMEOUT 0.5
 #define FACESCORE_TIMEOUT 0.5
+#define SPEECH_TIMEOUT 20
+
+struct Talker
+{
+    std_msgs::String speech;
+    ros::Time ts;
+}talker;
 
 uint32_t number_robot;
 vector<string> sorted_namespaces;
 ros::Time election_time;
 autonomy_human::human face_info;
-
+bool check_election;
 
 // ************** Debugging Visualization Parameters & Functions
 bool show_viz = true;
@@ -33,6 +40,7 @@ string lw_window = "Seletion Status";
 int lw_width = 400;
 int lw_height = 400;
 cv::Mat led_vis;
+string led_color;
 
 void clearWindow()
 {
@@ -41,31 +49,32 @@ void clearWindow()
     sprintf(buff,"%d FaceSore", face_info.faceScore);
     string str = buff;
     putText(led_vis,str,Point(25,25),CV_FONT_HERSHEY_PLAIN,1,CV_RGB(255,255,255));
+    led_color = "black";
 }
-
 void visualizeLed()
 {
     imshow(lw_window,led_vis);
     waitKey(1);
 }
-
 void greenLED()
 {
     circle(led_vis,Point(lw_height/2,lw_width/2),150,CV_RGB(0,255,0),-1);
+    led_color = "green";
 }
-
 void redLED()
 {
     circle(led_vis,Point(lw_height/2,lw_width/2),150,CV_RGB(255,0,0),-1);
+    led_color = "red";
 }
 void blueLED()
 {
     circle(led_vis,Point(lw_height/2,lw_width/2),150,CV_RGB(0,0,255),-1);
+    led_color = "blue";
 }
-
 void turnOffLED()
 {
     circle(led_vis,Point(lw_height/2,lw_width/2),150,CV_RGB(0,0,0),-1);
+    led_color = "black";
 }
 
 
@@ -132,11 +141,14 @@ std_msgs::Float32 average_fs;
 void electionResultsCallback(const autonomy_human::SortedNamespaces& msg)
 {
     //clearWindow();
-    number_robot = msg.num_robot;
-    sorted_namespaces = msg.namespaces;
-    election_time = msg.header.stamp;
-    ROS_INFO("The User wants: [ %d ]  robots",number_robot);
-    ROS_INFO("The first user is : [ %s ]  ",sorted_namespaces.at(0).c_str());
+    if(check_election)
+    {
+        number_robot = msg.num_robot;
+        sorted_namespaces = msg.namespaces;
+        election_time = msg.header.stamp;
+        ROS_INFO("The User wants: [ %d ]  robots",number_robot);
+        ROS_INFO("The first user is : [ %s ]  ",sorted_namespaces.at(0).c_str());
+    }
 }
 
 void humanCallback(const autonomy_human::human& msg)
@@ -151,6 +163,13 @@ void humanCallback(const autonomy_human::human& msg)
         ROS_INFO("My Average Face score: %f",average_fs.data);
     }
     ROS_INFO("My Face score: %d",face_info.faceScore);
+}
+
+void speechCallback (const std_msgs::String& msg)
+{
+    talker.speech.data = msg.data;
+    talker.ts = ros::Time::now();
+    ROS_INFO("*********************  I heard: %s",talker.speech.data.c_str());
 }
 
 void isElected(vector<string> sorted_ns, string& myname, bool& iselected, unsigned int& myposition)
@@ -180,6 +199,7 @@ int main(int argc, char **argv)
     namedWindow(lw_window); //visualization
     clearWindow(); //visualization
     turnOffLED();
+    check_election = true;
 
     string my_name = "Shokoofeh";
     unsigned int my_position = 0;
@@ -190,6 +210,7 @@ int main(int argc, char **argv)
     ros::Publisher aver_faceScore_pub = nh.advertise<std_msgs::Float32>("average_facescore",10);
     ros::Subscriber election_sub=nh.subscribe("/election",10,electionResultsCallback);
     ros::Subscriber human_sub=nh.subscribe("human",10,humanCallback);
+    ros::Subscriber speech_sub=nh.subscribe("/recognizer/output",10,speechCallback);
     ros::Rate loop_rate(10);
 
     while(ros::ok())
@@ -197,47 +218,60 @@ int main(int argc, char **argv)
         ROS_INFO("My name is: [ %s ]",my_name.c_str());
         ros::Duration last_election = ros::Time::now() - election_time;
         ros::Duration last_face_info = ros::Time::now() - face_info.header.stamp;
+        ros::Duration talker_last_ts = ros::Time::now() - talker.ts;
         ROS_INFO("Last election happened %f seconds ago",last_election.toSec());
-        std_msgs::Bool is_elected_msg;
 
         if(show_viz) visualizeLed();
 
-        if(last_face_info.toSec() > FACESCORE_TIMEOUT ) // What is going on?
-       {
-            face_info.faceScore = 0;
-            clearWindow();
-            turnOffLED();
-        }
-        else
+        if(check_election)
         {
-            if (last_election.toSec() < ELECTION_TIMEOUT)
+            if(last_face_info.toSec() > FACESCORE_TIMEOUT ) // I can not any Face! turn off the led and do not enter the election
+           {
+                face_info.faceScore = 0;
+                clearWindow();
+                turnOffLED();
+            }
+            else // I can see a face!
             {
-                isElected(sorted_namespaces,my_name,is_elected,my_position);
-                if(is_elected)
+                if (last_election.toSec() < ELECTION_TIMEOUT)
                 {
-                    clearWindow();
-                    greenLED();
+                    isElected(sorted_namespaces,my_name,is_elected,my_position);
+                    if(is_elected)
+                    {
+                        clearWindow();
+                        greenLED();
+                    }
+                    else if(!is_elected)
+                    {
+                        clearWindow();
+                        redLED();
+                    }
+
                 }
                 else
                 {
-                    clearWindow();
-                    redLED();
+                    is_elected = false;
+                    my_position = 0;
+                    blueLED();
+                }
+                if(talker_last_ts.toSec() < SPEECH_TIMEOUT)
+                {
+                    if((strcmp(talker.speech.data.c_str(),"yes") == 0) && ((strcmp(led_color.c_str(),"green")==0) || (strcmp(led_color.c_str(),"red") == 0)))
+                    {
+                        check_election = false;
+                    }
                 }
             }
-            else
-            {
-                is_elected = false;
-                my_position = 0;
-                blueLED();
-            }
-
         }
 
+        if ((strcmp(talker.speech.data.c_str(),"start election") == 0))
+        {
+            check_election = true;
+        }
 
         ROS_INFO("My position is: [ %d ]",my_position);
         ROS_INFO("Am I elected? [ %d ]",is_elected);
         ROS_INFO("My Face Score is: [%d]", face_info.faceScore);
-        //elected_pub.publish(is_elected_msg);
         aver_faceScore_pub.publish(average_fs);
         ros::spinOnce();
         loop_rate.sleep();
