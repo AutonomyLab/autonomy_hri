15475026f7eb6362e16687de788e68e2adb45ad7
Shokoofeh <sh.pourmehr@gmail.com>
2013-03-13 14:43:31 -0700

    rosbag script added.

---
+++ autonomy_human/log.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+    echo "Please provide the folder name."
+else
+    if [ -z "$2" ]
+        echo "Please provide namespace."
+    then
+    else
+        mkdir $2-$1
+        cd $2-$1
+        rosbag record $2/human $2/ardrone/image_raw $2/output_rgb_debug /election
+    fi
+fi
