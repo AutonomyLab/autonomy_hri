7e24ddc2150f1bfa3763dc9a58c8b8d4a642089a
Shokoofeh <sh.pourmehr@gmail.com>
2015-01-13 15:42:34 -0800

    Make proper changes to be compatible with HARK version 2.1.0

--- hark_msgs/scripts/old-version/demo.sh
+++ hark_msgs/scripts/old-version/demo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export BIN=$(rospack find hark_msgs)/scripts
+export BIN=$(rospack find hark_msgs)/scripts/
 export DATA=${BIN}/MultiSpeech.wav
 export DEVICE=plughw:1,0
 export CONF=${BIN}/kinect_tf.zip
@@ -9,12 +9,13 @@ if [ "$1" = "offline" ]
 then
     echo "Offline mode"
     echo ${BIN}/demoOffline.n ${DATA} ${CONF} Localization.txt \> log.txt
-    ${BIN}/demoOffline.n ${DATA} ${CONF} Localization.txt > log.txt
+         ${BIN}/demoOffline.n ${DATA} ${CONF} Localization.txt > log.txt
 elif [ "$1" = "online" ]
 then
     echo "Online mode"
-    echo ${BIN}/demoOnlineRos.n ${DEVICE} ${CONF} Localization.txt \> log.txt
-    ${BIN}/demoOnlineRos.n ${DEVICE} ${CONF} Localization.txt > log.txt
+    echo ${BIN}/demoOnline_Ros.n ${DEVICE} ${CONF} Localization.txt \> log.txt
+         ${BIN}/demoOnline_Ros.n ${DEVICE} ${CONF} Localization.txt > log.txt
 else
     echo "usage: sh demo.sh [online|offline]"
 fi
+
