48b3a92337319e7d8bb76a0a969e4e3d4a3e9431
Shokoofeh Pourmehr <sh.pourmehr@gmail.com>
2015-02-17 10:49:36 -0800

    Add ros param get

--- likelihood_grid/src/human_grid_node.cpp
+++ likelihood_grid/src/human_grid_node.cpp
@@ -17,31 +17,34 @@ int main(int argc, char** argv)
     bool get_sound_grid = true;
     bool get_torso_grid = true;
 
-//    ros::param::param("leg_node", get_leg_grid, true);
-    ros::param::param("leg_weight", human_grid.leg_weight, 2.5);
+    //    ros::param::param("/human_grid_node/leg_node", get_leg_grid, true);
+    //    ros::param::param("/human_grid_node/torso_node", get_torso_grid, true);
+    //    ros::param::param("/human_grid_node/sound_node", get_sound_grid, true);
+    ros::param::param("/human_grid_node/sound_weight", human_grid.sound_weight, 3.7);
+    ros::param::get("/human_grid_node/sound_weight", human_grid.sound_weight);
 
-//    ros::param::param("sound_node", get_sound_grid, true);
-    ros::param::param("sound_weight", human_grid.sound_weight, 3.7);
+    ros::param::param("/human_grid_node/torso_weight", human_grid.torso_weight, 3.8);
+    ros::param::get("/human_grid_node/torso_weight", human_grid.torso_weight);
 
-//    ros::param::param("torsi_node", get_torso_grid, true);
-    ros::param::param("torso_weight", human_grid.torso_weight, 3.8);
-
-
-        ros::Subscriber leg_grid_sub = n.subscribe("leg_probability", 10,
-                                                   &CHumanGrid::legCallBack,
-                                                   &human_grid);
-
-        ros::Subscriber sound_grid_sub = n.subscribe("sound_probability", 10,
-                                                     &CHumanGrid::soundCallBack,
-                                                     &human_grid);
+    ros::param::param("/human_grid_node/leg_weight", human_grid.leg_weight, 2.5);
+    ros::param::get("/human_grid_node/leg_weight", human_grid.leg_weight);
 
 
-        ros::Subscriber torso_grid_sub = n.subscribe("torso_probability", 10,
-                                                     &CHumanGrid::torsoCallBack,
-                                                     &human_grid);
+    ros::Subscriber leg_grid_sub = n.subscribe("leg_probability", 10,
+                                               &CHumanGrid::legCallBack,
+                                               &human_grid);
 
-        ros::Subscriber weights_sub = n.subscribe("/weights", 10,
-                                                  &CHumanGrid::weightsCallBack, &human_grid);
+    ros::Subscriber sound_grid_sub = n.subscribe("sound_probability", 10,
+                                                 &CHumanGrid::soundCallBack,
+                                                 &human_grid);
+
+
+    ros::Subscriber torso_grid_sub = n.subscribe("torso_probability", 10,
+                                                 &CHumanGrid::torsoCallBack,
+                                                 &human_grid);
+
+    ros::Subscriber weights_sub = n.subscribe("/weights", 10,
+                                              &CHumanGrid::weightsCallBack, &human_grid);
 
     while(ros::ok())
     {
